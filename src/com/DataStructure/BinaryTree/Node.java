3974bfb69c99580815cdd07185f1801706807c1b
surajrocks1997 <surajrocks1997@gmail.com>
2019-11-26 13:24:55 +0530

    Binary Tree added. Basic Hierarchy Structure

---
+++ src/com/DataStructure/BinaryTree/Node.java
@@ -0,0 +1,12 @@
+package com.DataStructure.BinaryTree;
+
+public class Node {
+    int key;
+    Node left, right;
+
+    public Node (int item) {
+        key = item;
+        left = right = null;
+    }        
+    
+}
---
+++ src/com/DataStructure/BinaryTree/Runner.java
@@ -0,0 +1,15 @@
+package com.DataStructure.BinaryTree;
+
+/**
+ * Runner
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+    }
+}
