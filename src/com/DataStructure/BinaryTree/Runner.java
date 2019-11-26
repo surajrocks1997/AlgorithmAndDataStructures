151aed2a1edd41bfd0d4e06326349c453911cc3a
surajrocks1997 <surajrocks1997@gmail.com>
2019-11-26 14:41:27 +0530

    Code Readability in Binary Tree -> Runner.java

--- src/com/DataStructure/BinaryTree/Runner.java
+++ src/com/DataStructure/BinaryTree/Runner.java
@@ -7,9 +7,13 @@ public class Runner {
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
+        tree.root = new Node(1);    
+            //will create 1 as a root with left and right child as null
         tree.root.left = new Node(2);
+            //will create left child of 1 as 2
         tree.root.right = new Node(3);
+            //will create left child of 1 as 3
         tree.root.left.left = new Node(4);
+            //will create left of left child of 1 as 4 so basically left child of 2
     }
 }
