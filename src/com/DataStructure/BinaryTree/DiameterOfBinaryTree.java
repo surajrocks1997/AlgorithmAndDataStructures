c4600ec2f05edd96981e91a596aaf9cd80cb56dd
surajrocks1997 <surajrocks1997@gmail.com>
2019-11-28 16:38:14 +0530

    few changes in DiameterOfBinaryTree.java added

--- src/com/DataStructure/BinaryTree/DiameterOfBinaryTree.java
+++ src/com/DataStructure/BinaryTree/DiameterOfBinaryTree.java
@@ -1,49 +1,43 @@
 package com.DataStructure.BinaryTree;
 
-public class DiameterOfBinaryTree {
-
+class DiameterOfBinaryTree{
 	public static void main(String[] args) {
 		CalDiam cal = new CalDiam();
-		cal.root = new Node(1);    
+		cal.root = new Node(1);
 		cal.root.left = new Node(2);
 		cal.root.right = new Node(3);
 		cal.root.left.left = new Node(4);
 		cal.root.left.right = new Node(5);
-		
-		System.out.println("Diameter: " + cal.calDiameter());
-		
+		System.out.println("Diameter: " + cal.diameter());
 	}
-
 }
 
 class CalDiam extends BinaryTree{
 
 	Node root;
 	
-	int calDiameter() {
+	public int diameter() {
 		return diameter(root);
 	}
 
-	private int diameter(Node root) {
-		if(root == null) {
+	private int diameter(Node node) {
+		if(node == null) {
 			return 0;
 		}
-		int lheight = height(root.left);
-		int rheight = height(root.right);
+		int lheight = height(node.left);
+		int rheight = height(node.right);
 		
-		int ldiameter = diameter(root.left);
-		int rdiameter = diameter(root.right);
+		int ldiameter = diameter(node.left);
+		int rdiameter = diameter(node.right);
 		
-		return Math.max(lheight + rheight + 1, Math.max(ldiameter, rdiameter));
+		return Math.max(1 + lheight + rheight, Math.max(ldiameter, rdiameter));
 	}
 
 	private int height(Node node) {
 		if(node == null) {
-			return 0; 
+			return 0;
 		}
 		return (1 + Math.max(height(node.left), height(node.right)));
 	}
-
-	
 	
 }
