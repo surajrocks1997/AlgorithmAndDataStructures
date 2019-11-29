239480ebdb17bb919cbee8626cf92def1fec857e
surajrocks1997 <surajrocks1997@gmail.com>
2019-11-29 20:23:48 +0530

    Working on single threaded inorder implementation in binary tree. Still Incomplete

---
+++ src/com/DataStructure/BinaryTree/SingleThreadedInorder.java
@@ -0,0 +1,47 @@
+package com.DataStructure.BinaryTree;
+/*
+ * Still Incomplete
+ */
+public class SingleThreadedInorder {
+
+	public static void main(String[] args) {
+		SingleThreadedInorderImplementation tree = new SingleThreadedInorderImplementation();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		
+		System.out.print("Inorder: ");
+		tree.Inorder();
+	}
+
+}
+
+class SingleThreadedInorderImplementation extends SingleThreadedBinaryTree{
+
+	Node root;
+	
+	public void Inorder() {
+	
+		Node current = leftmost(root);
+		while(current != null) {
+			System.out.print(current.data + " ");
+			
+			
+		}
+		
+	}
+
+	private Node leftmost(Node node) {
+		if(node == null) {
+			return null;
+		}
+		
+		while(node.left != null) {
+			node = node.left;
+		}
+		return node;
+	}
+	
+}
---
+++ src/com/DataStructure/BinaryTree/SingleThrededNode.java
@@ -0,0 +1,7 @@
+package com.DataStructure.BinaryTree;
+
+public class SingleThrededNode {
+	int data;
+	SingleThreadedInorder left, right;
+	boolean rightThread = false;
+}
