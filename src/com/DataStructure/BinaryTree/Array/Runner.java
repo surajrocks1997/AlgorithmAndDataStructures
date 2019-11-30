e7a1e831a8e6b5c5433a095609e936c0d23f82ea
surajrocks1997 <surajrocks1997@gmail.com>
2019-11-30 17:07:14 +0530

    Array Implementation Of Binary Tree added

--- src/com/DataStructure/BinaryTree/Array/Runner.java
+++ src/com/DataStructure/BinaryTree/Array/Runner.java
@@ -3,7 +3,18 @@ package com.DataStructure.BinaryTree.Array;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinaryTree tree = new BinaryTree();
+		tree.root("1");
+		tree.setLeft("2", 0);
+		tree.setRight("3", 0);
+		tree.setLeft("4", 1);
+		tree.setRight("5", 1);
+		tree.setLeft("6", 2);
+		tree.setRight("7", 2);
+		
+		tree.printTree();
+		
+		
 
 	}
 
