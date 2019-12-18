817c4c52b5b44f2145a1d5ddcf162532527e5192
surajrocks1997 <surajrocks1997@gmail.com>
2019-12-18 13:11:11 +0530

    Sorted Linked List to Balanced BST added

--- src/com/DataStructure/BinarySearchTree/SortedLinkedListToBalancedBST.java
+++ src/com/DataStructure/BinarySearchTree/SortedLinkedListToBalancedBST.java
@@ -3,8 +3,12 @@ package com.DataStructure.BinarySearchTree;
 public class SortedLinkedListToBalancedBST {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}
+
+class thisNode{
+	
 }
