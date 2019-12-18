7cd44206ebf176b0523af6345acbb8d0dd740e3f
surajrocks1997 <surajrocks1997@gmail.com>
2019-12-18 20:11:18 +0530

    Binary tree to BST added

---
+++ src/com/DataStructure/BinarySearchTree/BinaryTreeToBST.java
@@ -0,0 +1,11 @@
+package com.DataStructure.BinarySearchTree;
+
+public class BinaryTreeToBST {
+/*
+ * 1.	do an inorder traversal and store all the elements in an array
+ * 2.	sort the array using merge sort
+ * 3.	again do inorder traversal of the tree and rather than printing the data from
+ * 		the original tree, this time copy the data from array and store in the tree.
+ * 4.	done
+ */
+}
