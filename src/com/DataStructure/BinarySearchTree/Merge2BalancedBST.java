8271f447f6604f792d2cad970d8ba29749ebe961
surajrocks1997 <surajrocks1997@gmail.com>
2019-12-18 20:06:17 +0530

    Merge 2 balanced BST added

---
+++ src/com/DataStructure/BinarySearchTree/Merge2BalancedBST.java
@@ -0,0 +1,11 @@
+package com.DataStructure.BinarySearchTree;
+
+public class Merge2BalancedBST {
+/*
+ * 1.	Create 2 queue and using Inorder, add the elements to queue.
+ * 2.	Create an array of size(queue1 + queue2)
+ * 3.	Using queue.poll, insert the values of queue in increasing order in array
+ * 4.	With this sorted array, create BST with method in SortedArrayToBalancedBST.java
+ * 5.	Done.
+ */
+}
