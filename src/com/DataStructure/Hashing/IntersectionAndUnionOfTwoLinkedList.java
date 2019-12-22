d7393e14b57e1986c1ee47005d3273d0a7aa4ec7
surajrocks1997 <surajrocks1997@gmail.com>
2019-12-22 18:18:14 +0530

    intersection and union of two linked list

---
+++ src/com/DataStructure/Hashing/IntersectionAndUnionOfTwoLinkedList.java
@@ -0,0 +1,14 @@
+package com.DataStructure.Hashing;
+
+/*
+ * its a basic solution.
+ * check from link : https://www.geeksforgeeks.org/union-and-intersection-of-two-linked-lists/
+ */
+
+public class IntersectionAndUnionOfTwoLinkedList {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
