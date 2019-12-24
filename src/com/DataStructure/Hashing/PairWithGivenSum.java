694455f3060fc144fda31109ec3b2a97de7923bf
surajrocks1997 <surajrocks1997@gmail.com>
2019-12-24 16:42:08 +0530

    Pair with given sum using Hashing added

---
+++ src/com/DataStructure/Hashing/PairWithGivenSum.java
@@ -0,0 +1,27 @@
+package com.DataStructure.Hashing;
+
+import java.util.HashSet;
+
+public class PairWithGivenSum {
+
+	public static void main(String[] args) {
+		int A[] = { 1, 4, 45, 6, 10, 8 }; 
+		int sum = 9; 
+		printpairs(A, sum);
+	}
+
+	private static void printpairs(int[] a, int sum) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < a.length; i++) {
+			int temp = sum - a[i];
+			
+			if(set.contains(temp)) {
+				System.out.println(a[i] + " , " + temp);
+			}
+			set.add(a[i]);
+			
+		}
+		
+	}
+
+}
