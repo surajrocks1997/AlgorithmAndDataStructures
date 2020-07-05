c17f74ddbb8c5119006583e80209633c2c75c294
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-05 13:03:17 +0530

    runner class modified for adjList Graph

--- src/com/DataStructure/Graphs/AdjacencyList/Runner.java
+++ src/com/DataStructure/Graphs/AdjacencyList/Runner.java
@@ -3,7 +3,7 @@ package com.DataStructure.Graphs.AdjacencyList;
 public class Runner {
 
 	public static void main(String[] args) {
-		int V = 5;
+		int V = 4;
 		Graph graph = new Graph(V);
 //		graph.addEdge(graph, 0, 1);
 //		graph.addEdge(graph, 0, 4); 
@@ -15,14 +15,15 @@ public class Runner {
 		
 		graph.addEdge(graph, 0, 1); 
         graph.addEdge(graph, 0, 2); 
-        graph.addEdge(graph, 1, 2); 
+        graph.addEdge(graph, 1, 2);
+        graph.addEdge(graph, 2, 0);
         graph.addEdge(graph, 2, 3); 
-//        graph.addEdge(graph, 3, 3); 
+        graph.addEdge(graph, 3, 3); 
 		
 		graph.printGraph(graph);
 		
 		System.out.println("BFS: ");
-		graph.BFS(4);
+		graph.BFS(1);
 		System.out.println();
 		System.out.println("DFS: ");
 		graph.DFS();
