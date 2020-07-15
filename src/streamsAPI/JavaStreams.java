a645f6b9ce7080c97c1bff26bfe21de632e503b3
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-15 11:33:44 +0530

    StreamsAPI - working with text and CSV file

--- src/streamsAPI/JavaStreams.java
+++ src/streamsAPI/JavaStreams.java
@@ -1,7 +1,12 @@
 package streamsAPI;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -56,6 +61,55 @@ public class JavaStreams {
 			.filter(x -> x.startsWith("a"))
 			.forEach(System.out::println);
 		
+//		8. Streams rows from text file, sort, filter and print
+//		Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+//		bands
+//			.sorted()
+//			.filter(x -> x.length() > 13)
+//			.forEach(System.out::println);
+//		bands.close();
+		
+//		9. Stream rows from text file and save to List
+//		List<String> bands2 = Files.lines(Paths.get("bands.txt"))
+//				.filter(x -> x.contains("jit"))
+//				.collect(Collectors.toList());
+//		bands2.forEach(x -> System.out.println(x));
+		
+//		10. Stream rows from CSV file and count
+//		Stream<String> rows1 = Files.lines(Paths.get("data.txt"));
+//		int rowCount = (int)rows1
+//				.map(x -> x.split(","))
+//				.filter(x -> x.length == 3)
+//				.count();
+//		System.out.println("Row Count: " + rowCount);
+//		rows1.close();
+		
+//		11. Stream rows from csv file, parse data from rows
+//		Stream<String> rows2 = Files.lines(Paths.get("data.txt"));
+//		rows2
+//			.map(x -> x.split(","))
+//			.filter(x -> x.length == 3)
+//			.filter(x -> Integer.parseInt(x[1]) > 15)
+//			.forEach(x -> System.out.println(x[0] + " " + x[1] + " " + x[2]));
+//		rows2.close();
+		
+//		12. Stream rows from CSV file, store fields in HashMap
+//		Stream<String> rows3 = Files.lines(Paths.get("data.txt"));
+//		Map<String, Integer> map = new HashMap<String, Integer>();
+//		map = rows3
+//				.map(x -> x.split(","))
+//				.filter(x -> x.length == 3)
+//				.filter(x -> Integer.parseInt(x[1]) > 15)
+//				.collect(Collectors.toMap(
+//						x -> x[0], 
+//						x -> Integer.parseInt(x[1])));
+//		rows3.close();
+//		for(String key: map.keySet()) {
+//			System.out.println(key + " " + map.get(key));
+//		}
+		
+		
+			
 		
 	}
 }
